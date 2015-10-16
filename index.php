aa80c955eb3f11919e553843a365c24cc004e3fc
MaximSolodovnikov <MaximSolodovnikov@yandex.ua>
2015-10-16 14:04:53 +0300

    Change design

--- index.php
+++ index.php
@@ -24,19 +24,28 @@
 					</ul>
 				</div>
 				<div id="search">
+					<form action="" method="POST">
+						<input type="search" name="" />
+					</form>
 				</div>
 			</div>
 			<div id="rightblock">
+				<ul>
+					<li><a href="">Some category # 1</a></li>
+					<li><a href="">Some category # 2</a></li>
+					<li><a href="">Some category # 3</a></li>
+					<li><a href="">Some category # 4</a></li>
+					<li><a href="">Some category # 5</a></li>
+					<li><a href="">Some category # 6</a></li>
+				</ul>
 			</div>
 			<div id="slider">
 				<img src="img/city.jpg" alt="" />
 			</div>
-			
 			<div id="content">
 			It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 			</div>
-			<div id="footer">It's footer
-			</div>
+			<div id="footer"><span>Developed by Solodovnikov 2015</span></div>
 		</div>
 	</body>
 </html>
